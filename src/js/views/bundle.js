5c578818f50a5cafbb90bec0b14e701d5ae70c34
Poofikate <98_vit@mail.ru>
2022-05-30 17:48:50 +0300

    init

---
+++ src/js/views/bundle.js
@@ -0,0 +1,12 @@
+// scss
+import '@scss/inline/inline.scss';
+import '@scss/styles.scss';
+// svg
+import '@modules/_svg';
+
+// components
+import '@components/Modal';
+import '@components/Dropdown';
+
+// modules
+import '@modules/onLoad';
