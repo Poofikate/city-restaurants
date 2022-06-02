71cd67d746f284855bfe654bedc8ae17d63df2f7
arrkhip <arkhipovvladislav88@gmail.com>
2022-06-02 07:43:01 +0300

    Почистил код

--- src/js/components/ShowMore.js
+++ src/js/components/ShowMore.js
@@ -1,16 +1,14 @@
-const btn = document.querySelector('.js-charity__read-more');
-const content = document.querySelector('.js-charity__hidden');
+const $btn = document.querySelector('.js-charity__read-more');
+const $content = document.querySelector('.js-charity__hidden');
 
-btn.addEventListener('click', btnClick);
+$btn.addEventListener('click', btnClick);
 
 function btnClick() {
-  console.log(content.classList);
-
-  if (content.classList.contains('is-hidden')) {
-    btn.textContent = 'Скрыть';
+  if ($content.classList.contains('is-hidden')) {
+    $btn.textContent = 'Скрыть';
   } else {
-    btn.textContent = 'Читать далее';
+    $btn.textContent = 'Читать далее';
   }
 
-  content.classList.toggle('is-hidden');
+  $content.classList.toggle('is-hidden');
 }
